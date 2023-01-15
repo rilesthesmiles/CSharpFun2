52b41a8d67a28379984645c27168cbe3a055dcfb
rileymarshall <rileymarshall@RILEYMARSHA95DF>
2023-01-15 13:52:08 -0700

    Add project files.

---
+++ CSharpFun2/PrintStuffClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFun2
+{
+    class PrintStuffClass
+    {
+        private string language = "";
+        public PrintStuffClass(string temp)
+        {
+            language = temp;
+        }
+        public void PrintMyName(string inputName)
+        {
+            if (language == "EN") {
+                Console.WriteLine("Hello " + inputName);
+            }
+
+            if (language == "PI")
+            {
+                Console.WriteLine("Oi " + inputName);
+            }
+        }
+    }
+}
---
+++ CSharpFun2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace CSharpFun2
+{
+    public static class Program
+    {
+        public static void Main (string[] args)
+        {
+            PrintStuffClass ps;
+            string ?userName = "";
+            string ?languageSpoken = "";
+            Console.WriteLine("What language do you speak?");
+            languageSpoken = Console.ReadLine();
+            Console.WriteLine("Please enter your name");
+            userName = Console.ReadLine();
+
+            ps = new PrintStuffClass(languageSpoken);
+
+            ps.PrintMyName(userName);
+            
+
+        }
+
+        
+    }
+}
